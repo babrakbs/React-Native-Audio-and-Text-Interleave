28659146f76239193b4f80288faef2afded32371
babrak sohail <babrak.bs@gmail.com>
2025-09-01 18:22:19 +0500

    Maintained separate Color file

---
+++ src/Constants/index.js
@@ -0,0 +1,12 @@
+export const colors = {
+    primary: "#FACC15",
+    background: "#FFFFFF",
+    textPrimary: "#000000",
+    textSecondary: "#6B7280",
+    bubbleBackground: "#FFFFFF",
+    bubbleBorder: "#E5E7EB",
+    highlightBackground: "#E0E7FF",
+    highlightText: "#FACC15",
+    disabledText: "#9CA3AF",
+  };
+  
